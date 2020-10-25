92495c85cd253cbed25153938e0696c635cd26ed
ryokryok <mrozisan@gmail.com>
2020-10-26 01:29:53 +0900

    fetch from tmdb

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,48 +1,81 @@
-import React from "react";
-import { MovieData } from "../utils/types";
+import React, { useState } from "react";
+import { MovieData, MovieDataResponse } from "../utils/types";
 import mock_data from "../utils/mock_data_2010_10.json";
 import "dayjs/locale/ja";
 import dayjs from "dayjs";
+import useSWR from "swr";
+import { fetchAPI } from "../utils/api";
 
 export function Main() {
+  const { data, error } = useSWR<MovieDataResponse, any>("2010-10", fetchAPI);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
   return (
-    <main className="container mx-auto">
-      <Gallery movieData={mock_data.results} />
+    <main className="">
+      <Gallery movieData={data.results} />
     </main>
   );
 }
 
-export function Gallery({ movieData }: { movieData: MovieData[] }) {
+function SearchForm({
+  searchMonth,
+  setSearchMonth,
+}: {
+  searchMonth: string;
+  setSearchMonth: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    setSearchMonth(event.target.value);
+  }
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 justify-items-auto">
-      {movieData.map((md) => {
-        return (
-          <div
-            key={md.id}
-            className="relative max-w-sm rounded overflow-hidden shadow-lg"
-          >
-            <div className="bg-gray-400">
-              <img
-                src={`https://image.tmdb.org/t/p/original${md.poster_path}`}
-                alt={md.title}
-                className="object-contain h-64 w-full"
-              />
-            </div>
-            <div className="p-2 md:p-4">
-              <div className="font-bold text-lg mb-4">{md.title}</div>
-              <div className="text-gray-700 text-base">
-                {dayjs(md.release_date).format("YYYY年MM月DD日")}
+    <form className="w-full max-w-sm">
+      <div className="flex items-center border-b border-orange-500 py-2">
+        <input
+          className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          type="month"
+          value={searchMonth}
+          onChange={handleInput}
+        />
+      </div>
+    </form>
+  );
+}
+
+function Gallery({ movieData }: { movieData: MovieData[] }) {
+  return (
+    <div className="container mx-auto">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 justify-items-auto">
+        {movieData.map((md) => {
+          return (
+            <div
+              key={md.id}
+              className="relative max-w-sm rounded overflow-hidden shadow-lg"
+            >
+              <div className="bg-gray-400">
+                <img
+                  src={`https://image.tmdb.org/t/p/original${md.poster_path}`}
+                  alt={md.title}
+                  className="object-contain h-64 w-full"
+                />
               </div>
-              <TweetButton
-                title={md.title}
-                releaseYear={dayjs().year() - dayjs(md.release_date).year()}
-                releaseDate={dayjs(md.release_date).format("MM月DD日")}
-                id={md.id}
-              />
+              <div className="p-2 md:p-4">
+                <div className="font-bold text-lg mb-4">{md.title}</div>
+                <div className="text-gray-700 text-base">
+                  {dayjs(md.release_date).format("YYYY年MM月DD日")}
+                </div>
+                <TweetButton
+                  title={md.title}
+                  releaseYear={dayjs().year() - dayjs(md.release_date).year()}
+                  releaseDate={dayjs(md.release_date).format("MM月DD日")}
+                  id={md.id}
+                />
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
