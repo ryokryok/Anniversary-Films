b1073abd2f0234875f28aafa139a23ca97b96170
ryokryok <mrozisan@gmail.com>
2020-11-09 00:37:29 +0900

    feature modal, and add link to tmdb

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { Gallery, SearchForm } from "./Utils";
+import dayjs from "dayjs";
 
 export function Main() {
-  const [selectMonth, setSelectMonth] = useState("2010-10");
+  // initial month is 5 years ago
+  const [selectMonth, setSelectMonth] = useState(dayjs().subtract(5, "year").format("YYYY-MM"));
   return (
     <main className="">
       <SearchForm month={selectMonth} setMonth={setSelectMonth} />
--- utils/types.ts
+++ utils/types.ts
@@ -1,19 +1,19 @@
 export type MovieDataResponse = {
-  page: number
-  total_results: number
-  total_pages: number
-  results: MovieData[]
-}
+  page: number;
+  total_results: number;
+  total_pages: number;
+  results: MovieData[];
+};
 
 export type MovieData = {
-  id: number
-  title: string
-  poster_path: string | null
-  backdrop_path: string | null
-  original_title: string
-  original_language: string
-  overview: string
-  release_date: string
-  vote_average: number
-  popularity: number
-}
+  id: number;
+  title: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  original_title: string;
+  original_language: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  popularity: number;
+};
