bae30e9775749a63b4eeeac6eeca5b8fc9339e16
ryokryok <mrozisan@gmail.com>
2020-11-09 01:13:54 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,13 @@ export default function Home() {
         <title>nostalgic films</title>
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content="nostalgic films" />
-        <meta name="twitter:title" content="nostalgic films" />
         <meta
           property="og:description"
           content="映画を公開月で探せるサイトです。映画のデータはTMDBより使用しています。"
         />
+        <meta property="twitter:title" content="nostalgic films" />
+        <meta property="twitter:card" content="app" />
+        <meta property="twitter:image" content="/twitter-card.png" />
       </Head>
       <Main />
     </div>
