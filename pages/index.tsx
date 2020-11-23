114d00fa098cbffb81c3a222442baec82321340f
ryokryok <mrozisan@gmail.com>
2020-11-24 00:40:22 +0900

    fix layout, remove info button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { SearchForm, Gallery } from "../components/Utils";
 import { SiteHead } from "../components/Seo";
-import { useSelectMonth } from "../utils/hooks";
+import { useFetchMovieData, useSelectMonth } from "../utils/hooks";
 
 export default function Home() {
   const { month, handleChangeMonth } = useSelectMonth();
+  const { data, error } = useFetchMovieData(month);
 
   return (
     <div>
       <SiteHead month={month} />
       <SearchForm month={month} handleChangeMonth={handleChangeMonth} />
-      <Gallery month={month} />
+      {error ? <div>failed to load</div> : !data ? <div>loading...</div> : <Gallery data={data.results} />}
     </div>
   );
 }
